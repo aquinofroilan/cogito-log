854f45a4d44d368137b8110b4e8928807dfc09cd
Froilan J. Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-06-21 10:52:54 +0800

    feat: enhance authentication UI with LoginForm and SignIn page updates

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import LoginForm from "@/components/organisms/auth-forms/LoginForm";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui";
+import React from "react";
 
 const SignIn = () => {
-  return <main>SignIn</main>;
+    return (
+        <main>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Sign In</CardTitle>
+                    <CardDescription>Enter your credentials to access your account.</CardDescription>
+                </CardHeader>
+                <LoginForm />
+            </Card>
+        </main>
+    );
 };
 
 export default SignIn;
--- components/organisms/auth-forms/LoginForm.tsx
+++ components/organisms/auth-forms/LoginForm.tsx
@@ -1,5 +1,8 @@
+"use client";
 import {
     Button,
+    CardContent,
+    CardFooter,
     Form,
     FormControl,
     FormDescription,
@@ -7,8 +10,10 @@ import {
     FormItem,
     FormLabel,
     FormMessage,
+    Input,
 } from "@/components/ui";
 import { useForm } from "react-hook-form";
+import Link from "next/link";
 
 function LoginForm() {
     const loginForm = useForm({
@@ -28,47 +33,70 @@ function LoginForm() {
         console.log("Login data:", data);
     };
     return (
-        <Form {...loginForm}>
-            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-                <FormField
-                    control={control}
-                    name="email"
-                    render={({ field, fieldState }) => (
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <input type="email" placeholder="Enter your email" {...field} className="input" />
-                            </FormControl>
-                            <FormDescription>The email you used during registration.</FormDescription>
-                            <FormMessage>
-                                {fieldState.error ? fieldState.error.message : "Please enter a valid email address."}
-                            </FormMessage>
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={control}
-                    name="password"
-                    render={({ field, fieldState }) => (
-                        <FormItem>
-                            <FormLabel>Password</FormLabel>
-                            <FormControl>
-                                <input type="password" placeholder="Enter your password" {...field} className="input" />
-                            </FormControl>
-                            <FormDescription>The password you used during registration.</FormDescription>
-                            <FormMessage>
-                                {fieldState.error
-                                    ? fieldState.error.message
-                                    : "Your password must be at least 6 characters long."}
-                            </FormMessage>
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit" className="btn">
-                    Login
-                </Button>
-            </form>
-        </Form>
+        <>
+            <Form {...loginForm}>
+                <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+                    <CardContent>
+                        <FormField
+                            control={control}
+                            name="email"
+                            render={({ field, fieldState }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="email"
+                                            placeholder="Enter your email"
+                                            {...field}
+                                            className="input"
+                                        />
+                                    </FormControl>
+                                    <FormDescription>The email you used during registration.</FormDescription>
+                                    <FormMessage>
+                                        {fieldState.error
+                                            ? fieldState.error.message
+                                            : "Please enter a valid email address."}
+                                    </FormMessage>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={control}
+                            name="password"
+                            render={({ field, fieldState }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="password"
+                                            placeholder="Enter your password"
+                                            {...field}
+                                            className="input"
+                                        />
+                                    </FormControl>
+                                    <FormDescription>The password you used during registration.</FormDescription>
+                                    <FormMessage>
+                                        {fieldState.error
+                                            ? fieldState.error.message
+                                            : "Your password must be at least 6 characters long."}
+                                    </FormMessage>
+                                </FormItem>
+                            )}
+                        />
+                    </CardContent>
+                    <CardFooter>
+                        <Button type="submit" className="btn">
+                            Login
+                        </Button>
+                        <Button asChild>
+                            <Link href="/signup" className="btn-secondary">
+                                Sign Up
+                            </Link>
+                        </Button>
+                    </CardFooter>
+                </form>
+            </Form>
+        </>
     );
 }
 
