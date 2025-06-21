708a44c65db2456a33dc299d9a07b4075f3af1d8
Froilan J. Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-06-21 13:45:17 +0800

    feat: implement sign-up schema and enhance login action with validation and error handling

--- actions/auth.ts
+++ actions/auth.ts
@@ -1,34 +1,56 @@
 "use server";
-import type { LoginSchemaType } from "@/schemas";
+import { LoginSchema, SignUpSchema, type LoginSchemaType, type SignUpSchemaType } from "@/schemas";
 import { supabaseServerClient } from "@/utils/supabase/supabase";
 
 const loginAction = async (values: LoginSchemaType) => {
     try {
-        const { email, password } = values;
+        const validatedData = LoginSchema.safeParse(values);
+        if (!validatedData.success) return { success: false, message: "Invalid input data." };
+        if (values.email === "" || values.password === "")
+            return { success: false, message: "Email and password cannot be empty." };
+
+        const {
+            data: { email, password },
+        } = validatedData;
         const supabase = await supabaseServerClient();
         const { data, error } = await supabase.auth.signInWithPassword({
             email,
             password,
         });
 
-        if (error) {
-            return { success: false, message: error.message };
-        } else if (!data) {
-            return { success: false, message: "Unexpected login failure." };
-        }
+        if (error) return { success: false, message: error.message };
+        else if (!data) return { success: false, message: "Unexpected login failure." };
 
         return { success: true, data };
     } catch (err: unknown) {
         const errorMessage = err instanceof Error ? err.message : "An unknown error occurred.";
-        return {
-            success: false,
-            message: errorMessage,
-        };
+        return { success: false, message: errorMessage };
     }
 };
 
-const signUpAction = (email: string, password: string) => {
-    console.log("Signing up with:", { email, password });
+const signUpAction = async (values: SignUpSchemaType) => {
+    try {
+        const validatedData = SignUpSchema.safeParse(values);
+        if (!validatedData.success) return { success: false, message: "Invalid input data." };
+
+        const {
+            data: { email, password, confirmEmail, confirmPassword },
+        } = validatedData;
+        if (email !== confirmEmail) return { success: false, message: "Emails do not match." };
+        if (password !== confirmPassword) return { success: false, message: "Passwords do not match." };
+
+        const supabase = await supabaseServerClient();
+        const { data, error } = await supabase.auth.signUp({
+            email,
+            password,
+        });
+        if (error) return { success: false, message: error.message };
+        else if (!data) return { success: false, message: "Unexpected signup failure." };
+        return { success: true, data };
+    } catch (error: unknown) {
+        const errorMessage = error instanceof Error ? error.message : "An unknown error occurred.";
+        return { success: false, message: errorMessage };
+    }
 };
 
 export { loginAction, signUpAction };
