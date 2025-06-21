568b864d1821c4e5bce3016cea27b31508f60d81
Froilan J. Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-06-21 13:24:39 +0800

    feat: enhance login action with error handling and update LoginForm submission logic

--- actions/auth.ts
+++ actions/auth.ts
@@ -2,13 +2,27 @@
 import { supabaseServerClient } from "@/utils/supabase/supabase";
 
 const loginAction = async (email: string, password: string) => {
-    const supabase = await supabaseServerClient();
-    const { data, error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-    });
-    console.log("Login data:", data);
-    console.error("Login error:", error?.message);
+    try {
+        const supabase = await supabaseServerClient();
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        });
+
+        if (error) {
+            return { success: false, message: error.message };
+        } else if (!data) {
+            return { success: false, message: "Unexpected login failure." };
+        }
+
+        return { success: true, data };
+    } catch (err: unknown) {
+        const errorMessage = err instanceof Error ? err.message : "An unknown error occurred.";
+        return {
+            success: false,
+            message: errorMessage,
+        };
+    }
 };
 
 const signUpAction = (email: string, password: string) => {
